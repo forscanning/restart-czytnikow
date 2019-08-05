39756d30cdd72d70ce68e45e335022f93272e635
Adam Charusta <charustaadam95@gmail.com>
2019-08-05 19:54:38 +0200

    create layout, create prepare ip fn

---
+++ restart.js
@@ -0,0 +1,31 @@
+const startBtn = document.getElementById('startBtn');
+
+function makeArrayObjectFromContent(content) {
+    const arrayContent = content.split(',');
+    const objContent = [];
+    const filteredContent = arrayContent.filter(item => item.indexOf('Newland_') !== -1)
+
+    for (let i = 0; i < filteredContent.length; i++) {
+
+        let tab = filteredContent[i].split('_')
+        objContent[i] = { ip: tab[1] }
+    }
+    return objContent;
+}
+
+startBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    const content = document.getElementById('takeCSV').value;
+    const iframeUrl = makeArrayObjectFromContent(content);
+    iframeUrl.forEach((item, index) => {
+        const iFramePanel = document.createElement('IFRAME');
+        iFramePanel.setAttribute("src", item.ip);
+        document.body.appendChild(iFramePanel);
+        console.log('utowrzyłem');
+        document.body.removeChild(iFramePanel);
+        console.log((2 + 3) * index * index);
+        console.log('usunąłem');
+        if (index === iframeUrl.length - 1) return alert('Zrobione');
+    })
+})
+
